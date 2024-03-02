b940d86cdbed2bcf86ab9d0ba0c19b510a8b0ecb
Will <willianchua.wc@gmail.com>
2024-03-02 11:07:50 +0800

    Add Basic Display Logic
    User can now click buttons and input will be displayed. It presents the input similar to Google's calculator when clicking buttons. But, it currently does not support decimals

--- index.js
+++ index.js
@@ -16,16 +16,48 @@ function divide(x, y){
 
 function operate(x, y, operator){
     switch(operator){
-        case "+":
+        case SYMBOLS[0]:
             return add(x, y);
+        case SYMBOLS[1]:
+            return subtract(x, y);
+        case SYMBOLS[2]:
+            return multiply(x, y);
         default:
             return divide(x, y);
     }
 }
+const SYMBOLS = ["+", "−", "×", "÷"];
+let operands = [];
+let operators = [];
+const display = document.querySelector(".output");
+let hasPeriod = false;
 
+const calculator = document.querySelector(".grid_table")
+calculator.addEventListener("click", (e) => {
+    text = e.target.textContent;
+
+    if (text === "="){
+        // DO MATH
+    }
+    else if (display.textContent === "0" && text === "0"){
+    }
+    else if (SYMBOLS.includes(text) && SYMBOLS.includes(display.textContent[display.textContent.length - 1])){
+        equation = display.textContent.substring(0, display.textContent.length - 1);
+        equation += text;
+        display.textContent = equation;
+    }
+    else if (!SYMBOLS.includes(text) && display.textContent === "0"){
+        display.textContent = text;
+    }
+    else {
+        display.textContent += text;
+    }    
+})
 
 
 const AC = document.querySelector(".resetter")
 AC.addEventListener("click", () => {
-
+    display.textContent = "0";
+    operands = [];
+    operators = [];
 })
