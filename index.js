37f0bd18070171111f2e849cb04be87e80d7a0d4
Will <willianchua.wc@gmail.com>
2024-03-05 11:32:38 +0800

    Implement Calculator Logic
    planned to end within 1 hour, ended up finishing the logic in 1 hour and 30 minutes. The code of this commit  may be quite messy, specifically in the conditionals, but the calculator can handle decimals for each coefficient and does PEMDAS.

--- index.js
+++ index.js
@@ -27,57 +27,131 @@ function operate(x, y, operator){
     }
 }
 
-function calculate(eq){
+function extractValues(eq){
+    values = [];
     let num = "";
     for (let i = 0; i < eq.length; i++){
         if (!SYMBOLS.includes(eq[i])){
             num += eq[i];
         }
         else{
-            operands.push(Number(num));
-            operators.push(eq[i]);
+            values.push(Number(num));
+            values.push(eq[i]);
             num = "";
         }
     }
     if (num != ""){
-        operands.push(Number(num));
+        values.push(Number(num));
         num = "";
     }
-    console.log(operands);
+}
+
+function PEMDAS(){
+    let output = 0;
+
+    // OPERATE MULTIPLICATION AND DIVISION FIRST
+    for (let i = 0; i < values.length; i++){
+        if (values[i] == SYMBOLS[2] || values[i] == SYMBOLS[3]){
+            if (values[i+1] == 0){
+                return ERROR;
+            }
+
+            output = operate(values[i-1], values[i+1], values[i]);
+            v1 = values.slice(0, i-1);
+            v2 = values.slice(i+2);
+            v1.push(output);
+            values = v1.concat(v2);
+            i = i - 2;
+        }
+    }
+
+    // OPERATE ADDITION AND SUBTRACTION LAST
+    for (let i = 0; i < values.length; i++){
+        if (SYMBOLS.includes(values[i])){
+            output = operate(values[i-1], values[i+1], values[i]);
+            v1 = values.slice(0, i-1);
+            v2 = values.slice(i+2);
+            v1.push(output);
+            values = v1.concat(v2);
+            i = i - 2
+        }
+    }
+    return values[0];
+}
+
+function calculate(eq){
+    extractValues(eq);
+    let output =  PEMDAS();
+    return (output % 1 != 0) ? output.toFixed(2) : output;
 }
 
 const SYMBOLS = ["+", "−", "×", "÷"];
-let operands = [];
-let operators = [];
+const ERROR = "CANNOT DIVIDE BY ZERO.";
 const display = document.querySelector(".output");
-let hasPeriod = false;
-
+let values = [];
+let coefficient = "";
 const calculator = document.querySelector(".grid_table")
 calculator.addEventListener("click", (e) => {
     text = e.target.textContent;
+    
+    if (text === "="){
+        if (SYMBOLS.includes(display.textContent[display.textContent.length - 1])){
+            let equation = display.textContent.substring(0, display.textContent.length - 1);
+            equation += text;
+            display.textContent = equation;
+        }
 
-    if (SYMBOLS.includes(text) && SYMBOLS.includes(display.textContent[display.textContent.length - 1])){
-        equation = display.textContent.substring(0, display.textContent.length - 1);
+        display.textContent = calculate(display.textContent);
+        coefficient = display.textContent;
+    }
+    else if (display.textContent == ERROR){
+        display.textContent = "0";
+        coefficient = "";
+    }
+    else if (display.textContent[display.textContent.length - 1] == '.' && 
+             SYMBOLS.includes(text)){
+        let equation = display.textContent.substring(0, display.textContent.length - 1);
         equation += text;
         display.textContent = equation;
+        console.log(`COEFFICIENT: ${coefficient}`);
+        coefficient = coefficient.substring(0, coefficient.length-1);
+        console.log(`COEFFICIENT: ${coefficient}`);
     }
+    // Replace operator
+    else if (SYMBOLS.includes(display.textContent[display.textContent.length - 1]) &&
+             SYMBOLS.includes(text)){
+        let equation = display.textContent.substring(0, display.textContent.length - 1);
+        equation += text;
+        display.textContent = equation;
+        coefficient = "";
+    } 
     else if (!SYMBOLS.includes(text) && display.textContent === "0"){
         display.textContent = text;
-    }
-    else if (display.textContent === "0" && text === "0"){
+        coefficient = text;
     } 
-    else if (text === "="){
-        calculate(display.textContent);
+    else if (display.textContent === "0" && text === "0"){
+        coefficient = "0";
+    } 
+    else if (coefficient.includes('.') && text == '.'){
+    }
+    else if (coefficient == "" && text == '.'){
+        coefficient = "0.";
+        display.textContent += "0.";
     }
     else {
         display.textContent += text;
-    }    
+        coefficient += text;
+    }
+    
+    if (SYMBOLS.includes(text)){
+        coefficient = "";
+    }
+    console.log(coefficient);
 })
-
 
 const AC = document.querySelector(".resetter")
 AC.addEventListener("click", () => {
     display.textContent = "0";
-    operands = [];
-    operators = [];
+    values = [];
+    coefficient = ""
 })
